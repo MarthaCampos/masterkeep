d8936847de4bfd787c333ce0d6f25afa0f7b3e4c
MarthaCampos <martha.campos6@gmail.com>
2018-01-24 15:55:05 -0200

    commit inicial

---
+++ src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  menuVisivel=false;
+  dados = {titulo: '', conteudo: ''};
+  lista = [];
+
+  constructor(){
+    let dadosString = localStorage.getItem('cards');
+
+    if(dadosString){
+      this.lista = JSON.parse(dadosString);
+    }
+  }
+
+  alternarMenu(){
+    if(this.menuVisivel){
+      this.menuVisivel = false;
+    } else{
+      this.menuVisivel = true;
+    }
+  }
+
+  inserir(){
+    this.lista.push(this.dados);
+    this.dados = {titulo: '', conteudo: ''};
+    // console.log(this.lista);
+
+    let dadosString = JSON.stringify(this.lista);
+
+    localStorage.setItem('cards', dadosString);
+  }
+limpar(){
+  this.lista = [];
+  localStorage.clear();
+}
+}
